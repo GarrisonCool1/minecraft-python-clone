601e8b1f2db7919365e1d449e3b4b87ed0e01fcb
GarrisonCool1 <126790866+GarrisonCool1@users.noreply.github.com>
2024-06-12 16:14:17 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{pkgs}: {
+  deps = [
+    pkgs.unzip
+    pkgs.openal
+    pkgs.libpulseaudio
+    pkgs.libGLU
+    pkgs.libGL
+    pkgs.gtk2-x11
+    pkgs.gdk-pixbuf
+    pkgs.freetype
+    pkgs.fontconfig
+    pkgs.ffmpeg-full
+  ];
+}
